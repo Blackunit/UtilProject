172e2ceb9908c62daceb08523f222e4e2fd72a89
dx <1796581057@qq.com>
2018-05-23 22:29:56 +0800

    修整FileUtil2和BaseActivity

--- app/src/main/java/com/example/dx/utilproject/activity/BaseActivity.java
+++ app/src/main/java/com/example/dx/utilproject/activity/BaseActivity.java
@@ -5,24 +5,35 @@ import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
- * 如果要考虑兼容Android3.0之前的版本也使用Fragment
- * 可以考虑让BaseActivity继承自FragmentActivity
- * 然后使用getSupportFragmentManager()方法获取FragmentManager
+ *
+ * @author dx
+ *         <p>
+ *         BaseActivity是项目中所有的Activity的基类
+ *         将常用的一些方法,简单封装了一下
+ *         <p>
+ *         注意:
+ *         如果要考虑兼容Android3.0之前的版本也使用Fragment
+ *         可以考虑让BaseActivity继承自FragmentActivity
+ *         然后使用getSupportFragmentManager()方法获取FragmentManager
  */
 public abstract class BaseActivity extends Activity {
-    private static Stack<Activity> activityStack=new Stack<>();
+    private static final String TAG = "BaseActivity";
+    private static LinkedList<Activity> activityList = new LinkedList<>();
     private static Activity visibleActivity;
     private Dialog progressDialog;
     private Fragment currentFragment;
@@ -35,118 +46,153 @@ public abstract class BaseActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Activity设置为无标题栏
+
+        // Activity设置为无标题栏的状态
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //将Activity推入栈中,进行记录
-        activityStack.push(this);
+        activityList.addLast(this);
 
         /*
-        下面个方法的调用顺序不能改变
+        下面个方法的调用顺序不能改变，否则可能会对其他Activity的逻辑造成影响
+        如果需要新增Activity,注意这三个方法的执行顺序.
          */
-        //初始化布局View
         initView(savedInstanceState);
-        //初始化数据
         initData();
-        //初始化各种Listener
         initListener();
     }
 
     /**
      * 初始化view，该方法中必须要有 setContentView(R.layout.activity_main);
+     * 在 {@link #initData()} 方法和 {@link #initListener()} 方法之前执行
+     *
      * @param savedInstanceState 就是onCreate方法参数中的savedInstanceState
      *                           方便恢复Activity后，恢复之前的数据
      */
     public abstract void initView(@Nullable Bundle savedInstanceState);
 
     /**
-     * 初始化数据
+     * 初始化数据,在 {@link #initView(Bundle)} 方法之后执行
+     * 在 {@link #initListener()} 方法之前执行
      */
     public abstract void initData();
+
     /**
-     * 初始化各种监听器
+     * 初始化各种监听器,在 {@link #initView(Bundle)} 方法和 {@link #initData()} 方法之后执行
      */
     public abstract void initListener();
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(activityStack.contains(this)){
-            activityStack.remove(this);
-        }
+        activityList.remove(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        visibleActivity =this;
+        visibleActivity = this;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        visibleActivity =null;
+        visibleActivity = null;
     }
 
     /**
      * 获取当前可见的Activity，如果当前没有可见的Activity，就返回null
      * 判定规则是，只要Activity调用了onPause方法，就不可见了
-     * @return
+     *
+     * @return 当前可见的Activity, 如果当前没有可见的Activity, 就返回null
      */
-    public static Activity getVisibleActivity(){
+    public static Activity getVisibleActivity() {
         return visibleActivity;
     }
 
-    /**
-     * 获取最顶部的Activity
-     * @return
-     */
-    public static Activity getTopActivity(){
-        if(!activityStack.empty()) {
-            return activityStack.peek();
-        }else {
+    public static Activity getTopActivity() {
+        if (activityList.size() > 0) {
+            return activityList.getLast();
+        } else {
             return null;
         }
     }
 
-    public void startActivity(Class<?> targetActivityCls){
-        startActivity(targetActivityCls,null);
+    public static Class<? extends Activity> getTopActivityClass() {
+        if (activityList.size() > 0) {
+            return activityList.getLast().getClass();
+        } else {
+            return null;
+        }
     }
-    public void startActivity(Class<?> targetActivityCls, Bundle bundle){
-        Intent intent=new Intent(this,targetActivityCls);
-        if (bundle!=null) {
+
+    public void startActivity(Class<?> targetActivityCls) {
+        startActivity(targetActivityCls, null);
+    }
+
+    public void startActivity(Class<?> targetActivityCls, Bundle bundle) {
+        Intent intent = new Intent(this, targetActivityCls);
+        if (bundle != null) {
             intent.putExtras(bundle);
         }
-
         startActivity(intent);
-        //使用动画效果
+
+        //使用动画效果,被启动界面会从左到右滑动过来
         overridePendingTransition(android.R.anim.slide_in_left, 0);
     }
-    public void showToast(String msg){
-        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+
+    /**
+     * 杀死指定的Activity,例如:killActivity(MainActivity.class);
+     *
+     * @param targetActivityCls 目标界面
+     */
+    public void killActivity(Class<?> targetActivityCls) {
+        int targetActivityIndex = -1;
+        for (int i = 0; i < activityList.size(); i++) {
+            Activity activity = activityList.get(i);
+            if (activity.getClass().getName().equals(targetActivityCls.getName())) {
+                targetActivityIndex = i;
+                break;
+            }
+        }
+        if (targetActivityIndex >= 0) {
+            activityList.get(targetActivityIndex).finish();
+            activityList.remove(targetActivityIndex);
+            Log.d(TAG, "成功杀死：" + targetActivityCls.getName());
+        }
     }
-    public void showToastLong(String msg){
-        Toast.makeText(this,msg, Toast.LENGTH_LONG).show();
+
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-    public String getResourcesString(int rid){
+
+    public void showToastLong(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public String getResourcesString(int rid) {
         return getResources().getString(rid);
     }
-    protected static void finishAll(){
-        while (!activityStack.empty()) {
-            Activity activity=activityStack.pop();
-            activity.finish();
+
+    protected static void finishAll() {
+        for (int i = 0; i < activityList.size(); i++) {
+            activityList.get(i).finish();
+        }
+        activityList.clear();
+    }
+
+    public void closeInputMethod() {
+        View view = getWindow().peekDecorView();
+        if (view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (inputMethodManager != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
         }
     }
-    public void closeInputMethod(){
-        View view=getWindow().peekDecorView();
-        if (view!=null){
-            InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
-    public void showProgressDialog(String msg){
+
+    public void showProgressDialog(String msg) {
         //经过了测试，ProgressDialog可以复用
-        if (progressDialog==null){
-            progressDialog=createProgressDialog(msg);
+        if (progressDialog == null) {
+            progressDialog = createProgressDialog(msg);
         }
         progressDialog.show();
     }
@@ -157,8 +203,9 @@ public abstract class BaseActivity extends Activity {
         progressDialog.show();
         return progressDialog;
     }
-    public void dismissProgressDialog(){
-        if (progressDialog!=null){
+
+    public void dismissProgressDialog() {
+        if (progressDialog != null) {
             progressDialog.dismiss();
         }
     }
@@ -166,13 +213,15 @@ public abstract class BaseActivity extends Activity {
     @Override
     public void finish() {
         super.finish();
+
         //使用默认动画
         closeInputMethod();
         overridePendingTransition(0, android.R.anim.slide_out_right);
     }
+
     /**
      * Fragment替换(当前destrory,新的create)
-     * */
+     */
     public void fragmentReplace(int target, Fragment toFragment, boolean backStack) {
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -192,18 +241,22 @@ public abstract class BaseActivity extends Activity {
     public void smartFragmentReplace(int target, Fragment toFragment) {
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
+
         // 如有当前在使用的->隐藏当前的
         if (currentFragment != null) {
             transaction.hide(currentFragment);
         }
         String toClassName = toFragment.getClass().getSimpleName();
+
         // toFragment之前添加使用过->显示出来
         if (manager.findFragmentByTag(toClassName) != null) {
             transaction.show(toFragment);
-        } else {// toFragment还没添加使用过->添加上去
+        } else {
+            // toFragment还没添加使用过->添加上去
             transaction.add(target, toFragment, toClassName);
         }
         transaction.commit();
+
         // toFragment更新为当前的
         currentFragment = toFragment;
     }
--- app/src/main/java/com/example/dx/utilproject/file/FileUtil2.java
+++ app/src/main/java/com/example/dx/utilproject/file/FileUtil2.java
@@ -36,24 +36,35 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 /**
- * 使用该类时，最好声明文件修改权限，并动态申请权限
- * android.permission.MOUNT_UNMOUNT_FILESYSTEMS
- * android.permission.WRITE_EXTERNAL_STORAGE
- * android.permission.READ_EXTERNAL_STORAGE
- * <p>
- * 获取文件目录的方法，得到的目录有可能不存在，建议在该目录下进行读写操作时，先创建一下(调用FileUtil.mkdirs方法)
+ *
+ * @author dx
+ *         <p>
+ *         FileUtil2类是重新封装的一个文件操作相关的工具类，用于代替原有的FileUtil：
+ *         主要功能：包括路径的获取、路径或文件的创建、文件的读写、md5、
+ *         压缩和解压、流的的读取等。
+ *         <p>
+ *         注意：
+ *         使用该类时，最好声明文件修改权限，并动态申请权限
+ *         android.permission.MOUNT_UNMOUNT_FILESYSTEMS
+ *         android.permission.WRITE_EXTERNAL_STORAGE
+ *         android.permission.READ_EXTERNAL_STORAGE
+ *         <p>
+ *         获取文件目录的方法，得到的目录有可能不存在，建议在该目录下进行读写操作时，先创建一下(调用FileUtil.mkdirs方法)
  */
 public class FileUtil2 {
     private static final String TAG = "FileUtil2";
     /**
-     * 默认字符编码
+     * 默认字符编码,主要涉及到以下方法
+     * {@link #write(String, String, boolean)}
+     * {@link #read(String)}
      */
     private static final String DEFAULT_CHARSET = "UTF-8";
 
-    private static Context mContext = MyApplication.getContext().getApplicationContext();
+    private static Context mContext = MyApplication.getContext();
 
     /**
      * 各种文件类型,主要服务于这个方法 {@link #fileType}
+     * 用于给不同的文件归类
      */
     public static class FileType {
         /**
@@ -98,44 +109,52 @@ public class FileUtil2 {
         public static final int VIDEO = 9;
     }
 
-    public static abstract class InputStreamReadListener{
+    /**
+     * 读取输入流的监听器,可以实时监听读流的进度
+     */
+    public static abstract class InputStreamReadListener {
         /**
          * 由于InputStream的available方法不可靠(特别是当网络请求的时候)，所以这里决定让调用者传入流的大小
          * 返回的值如果小于等于0，将会忽略
+         *
          * @return InputStream流的大小，单位是bytes
          */
-        public long length(){
+        public long length() {
             return 0;
         }
 
         /**
          * 已经读取的数据大小,如果length()方法返回了一个有效的值，这里将会计算progress
-         * @param bytes 单位为bytes
-         *              如果这里的bytes的值超出了length()的值，将会输出警告日志
+         *
+         * @param bytes    单位为bytes
+         *                 如果这里的bytes的值超出了length()的值，将会输出警告日志
          * @param progress 取值范围是0.0到100.0,
          *                 如果length()没有给出有效值，这里的progress的值永远为0.0
          *                 如果这里的bytes的值超出了length()的值，这里将会100.0，并输出警告日志
          */
-        public void onRead(long bytes,float progress){
+        public void onRead(long bytes, float progress) {
 
         }
 
         /**
          * 读取完成，并给出总共读取的大小
+         *
          * @param bytes 单位为bytes
          */
-        public void onComplete(long bytes){
+        public void onComplete(long bytes) {
 
         }
 
         /**
          * 读取出现异常
+         *
          * @param msg 错误信息
          */
-        public void onError(String msg){
+        public void onError(String msg) {
 
         }
     }
+
     /**
      * @return 获取设备的sd卡的根目录
      */
@@ -144,7 +163,7 @@ public class FileUtil2 {
     }
 
     /**
-     * 获取sd卡下指定目录：
+     * 获取sd卡根目录下指定的目录：
      * 例如：getExternalStoragePublicPath(Environment.DIRECTORY_DOWNLOADS)将会返回/sdcard/Download路径
      * getExternalStoragePublicPath("test")将会返回/sdcard/test路径
      *
@@ -153,7 +172,7 @@ public class FileUtil2 {
      *             {@link Environment#DIRECTORY_NOTIFICATIONS}, {@link Environment#DIRECTORY_PICTURES},
      *             {@link Environment#DIRECTORY_MOVIES}, {@link Environment#DIRECTORY_DOWNLOADS},
      *             {@link Environment#DIRECTORY_DCIM}, {@link Environment#DIRECTORY_DOCUMENTS}
-     * @return
+     * @return /sdcard/xxx路径,xxx取决于方法传入的字符串
      */
     public static File getExternalStoragePublicPath(@Nullable String type) {
         return Environment.getExternalStoragePublicDirectory(type);
@@ -227,15 +246,17 @@ public class FileUtil2 {
      *
      * @param pathname 具体文件目录
      * @return 对应目录的文件对象
-     * @throws IOException
+     * @throws IOException 当对应的目录下存在一个路径相应的文件,将会抛出此异常
      */
     public static File mkdirs(String pathname) throws IOException {
         File file = new File(pathname);
         if (!file.exists()) {
-            file.mkdirs();
+            if (!file.mkdirs()) {
+                throw new IOException("Failed to create the path!");
+            }
         }
         if (file.isFile()) {
-            throw new IOException("当前路径下存在一个同名的文件");
+            throw new IOException("There is a file with the same name in the current path!");
         }
         return file;
     }
@@ -268,6 +289,7 @@ public class FileUtil2 {
      * @return 创建完成的文件
      */
     public static File createFile(String pathname) {
+
         //先创建路径
         mkdirsFilePath(pathname);
 
@@ -287,15 +309,31 @@ public class FileUtil2 {
      *
      * @param pathname 具体文件目录
      */
-    public static void deleteFile(String pathname) {
+    public static boolean deleteFile(String pathname) {
         File file = new File(pathname);
-        file.deleteOnExit();
+        return deleteDir(file);
+    }
+
+    /**
+     * 递归删除文件
+     * @param dirPath 可以是文件,也可以是目录
+     * @return 是否删除成功,只要文件夹下有一个文件无法删除成功,就返回false
+     */
+    private static boolean deleteDir(File dirPath){
+        if (dirPath.isDirectory()){
+            String[] childrenFiles=dirPath.list();
+            for (int i=0;i<childrenFiles.length;i++){
+                boolean delSuccess=deleteDir(new File(dirPath,childrenFiles[i]));
+                if (!delSuccess){
+                    return false;
+                }
+            }
+        }
+        return dirPath.delete();
     }
 
     /**
      * 判断当前是否位于主线程中
-     *
-     * @return
      */
     private static boolean isMainThread() {
         return Looper.getMainLooper().getThread().getId() == Thread.currentThread().getId();
@@ -306,29 +344,34 @@ public class FileUtil2 {
             logW(methodName + "方法最好放在子线程中运行");
         }
     }
+
     /**
      * 通过总的流和已读的IO流计算进度
-     * @param allBytes 总的流大小
+     *
+     * @param allBytes  总的流大小
      * @param readBytes 目前已读的大小
      * @return 读取的进度，在0.0到100.0范围内，只保留了一位小数
      */
-    private static float computeProgress(long allBytes,long readBytes){
-        if (allBytes<=0){
+    private static float computeProgress(long allBytes, long readBytes) {
+        if (allBytes <= 0) {
             return 0.0f;
         }
-        if (readBytes>=allBytes){
+        if (readBytes >= allBytes) {
             return 100.0f;
         }
-        float progress=(float)readBytes*100/allBytes;
-        //这里操作了一下，然返回结果只保留一位小数
-        progress=(float)(Math.round(progress*10))/10;
+        float progress = (float) readBytes * 100 / allBytes;
+
+        //这里将返回结果强制保留一位小数
+        progress = (float) (Math.round(progress * 10)) / 10;
+
         //由于流快读取完成时,如果进度是99.95，取一位小数就变成了100.0，所以这里单独处理了一下
-        if (progress<100.0f){
+        if (progress < 100.0f) {
             return progress;
-        }else {
+        } else {
             return 99.9f;
         }
     }
+
     /**
      * 将输入流存入文件中，该方法会自动关闭InputStream流
      *
@@ -336,9 +379,10 @@ public class FileUtil2 {
      * @param pathname 文件路径，如果文件不存在，将会自动创建
      * @return 是否存入成功
      */
-    public static boolean saveFile(@NonNull InputStream is, @NonNull String pathname){
-        return saveFile(is,pathname,null);
+    public static boolean saveFile(@NonNull InputStream is, @NonNull String pathname) {
+        return saveFile(is, pathname, null);
     }
+
     /**
      * 将输入流存入文件中，该方法会自动关闭InputStream流
      *
@@ -353,27 +397,31 @@ public class FileUtil2 {
 
         File file = createFile(pathname);
         if (!file.exists()) {
-            listener.onError("文件创建失败,pathname="+pathname);
+            if (listener != null) {
+                listener.onError("文件创建失败,pathname=" + pathname);
+            }
             return false;
         }
         try {
-            FileOutputStream fos=new FileOutputStream(file);
-            return readWriteStream(is,fos,listener);
+            FileOutputStream fos = new FileOutputStream(file);
+            return readWriteStream(is, fos, listener);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return false;
     }
+
     /**
      * 将输入流写入输出流中，该方法会自动关闭InputStream流和OutputStream流
      *
-     * @param is       输入流，执行完该方法后，会自动关闭InputStream流
-     * @param os       输出流，执行完该方法后，会自动关闭OutputStream流
+     * @param is 输入流，执行完该方法后，会自动关闭InputStream流
+     * @param os 输出流，执行完该方法后，会自动关闭OutputStream流
      * @return 是否写入成功
      */
-    public static boolean readWriteStream(@NonNull InputStream is, @NonNull OutputStream os){
-        return readWriteStream(is,os,null);
+    public static boolean readWriteStream(@NonNull InputStream is, @NonNull OutputStream os) {
+        return readWriteStream(is, os, null);
     }
+
     /**
      * 将输入流写入输出流中，该方法会自动关闭InputStream流和OutputStream流
      *
@@ -382,17 +430,19 @@ public class FileUtil2 {
      * @param listener 读取流的监听器，具体的回调逻辑参见 {@link InputStreamReadListener}
      * @return 是否写入成功
      */
-    public static boolean readWriteStream(@NonNull InputStream is, @NonNull OutputStream os, @Nullable InputStreamReadListener listener){
+    public static boolean readWriteStream(@NonNull InputStream is, @NonNull OutputStream os, @Nullable InputStreamReadListener listener) {
         checkThread("readWriteStream");
 
-        if (listener==null){
+        if (listener == null) {
+
             //如果listener传入的是null，这里就手动创建一个，防止后面的操作总是要判断listener是否为null
-            listener=new InputStreamReadListener() {};
+            listener = new InputStreamReadListener() {
+            };
         }
-        long streamLength=listener.length();
-        long readLength=0;
-        float lastProgress=0.0f;
-        float progress=0.0f;
+        long streamLength = listener.length();
+        long readLength = 0;
+        float lastProgress = 0.0f;
+        float progress;
 
         byte[] buff = new byte[2048];
         int count;
@@ -401,21 +451,22 @@ public class FileUtil2 {
                 os.write(buff, 0, count);
                 os.flush();
 
-                readLength+=count;
-                progress=computeProgress(streamLength,readLength);
+                readLength += count;
+                progress = computeProgress(streamLength, readLength);
+
                 //这里进行了进度的判断，防止多余的回调
-                if(progress<=0.0f){
-                    listener.onRead(readLength,progress);
-                }else if (progress>lastProgress){
-                    lastProgress=progress;
-                    listener.onRead(readLength,progress);
+                if (progress <= 0.0f) {
+                    listener.onRead(readLength, progress);
+                } else if (progress > lastProgress) {
+                    lastProgress = progress;
+                    listener.onRead(readLength, progress);
                 }
             }
-            if (streamLength>0&&streamLength>readLength){
+            if (streamLength > 0 && streamLength > readLength) {
                 logW("读取到的流的大小，未达到InputStreamReadListener中length()给出的大小，" +
                         "导致InputStreamReadListener的onRead方法中的进度是不可信的");
             }
-            if (streamLength>0&&streamLength<readLength){
+            if (streamLength > 0 && streamLength < readLength) {
                 logW("读取到的流的大小，已超出InputStreamReadListener中length()给出的大小，" +
                         "导致InputStreamReadListener的onRead方法中的进度是不可信的");
             }
@@ -423,7 +474,7 @@ public class FileUtil2 {
             return true;
         } catch (IOException e) {
             e.printStackTrace();
-            listener.onError("IO异常,errorMsg="+e.toString());
+            listener.onError("IO异常,errorMsg=" + e.toString());
         } finally {
             try {
                 os.close();
@@ -440,9 +491,10 @@ public class FileUtil2 {
      *
      * @param data     要存入的数据
      * @param pathname 文件路径
-     * @return
+     * @return 是否写入成功
      */
     public static boolean write(String data, String pathname) {
+
         //默认为覆盖写入
         return write(data, pathname, false);
     }
@@ -461,21 +513,21 @@ public class FileUtil2 {
         //先创建路径
         mkdirsFilePath(pathname);
 
-        OutputStreamWriter writer = null;
-        BufferedWriter out = null;
+        OutputStreamWriter os;
+        BufferedWriter writer = null;
         try {
-            writer = new OutputStreamWriter(new FileOutputStream(pathname, append), Charset.forName(DEFAULT_CHARSET));
-            out = new BufferedWriter(writer);
-            out.write(data);
-            out.flush();
+            os = new OutputStreamWriter(new FileOutputStream(pathname, append), Charset.forName(DEFAULT_CHARSET));
+            writer = new BufferedWriter(os);
+            writer.write(data);
+            writer.flush();
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         } finally {
-            if (out != null) {
+            if (writer != null) {
                 try {
-                    out.flush();
-                    out.close();
+                    writer.flush();
+                    writer.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -502,11 +554,14 @@ public class FileUtil2 {
             StringBuilder sb = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+                sb.append(line);
+                sb.append("\n");
+            }
+            //将最后一个多余的"\n"符号删除
+            if (sb.length()>0){
+                sb.deleteCharAt(sb.length()-1);
             }
             return sb.toString();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -560,8 +615,8 @@ public class FileUtil2 {
      * 获取文件的扩展名,以小写的方式返回
      * 例如原来的文件是xxx.PNG，该方法会返回.png
      *
-     * @param fileName
-     * @return
+     * @param fileName 文件名字,可以不用包含路径
+     * @return 文件的后缀名, 例如.mp4 .ppt .jpg等
      */
     public static String getFileExtension(String fileName) {
         if (TextUtils.isEmpty(fileName)) {
@@ -581,6 +636,7 @@ public class FileUtil2 {
      * @return 返回文件类型, 目前支持这几种文件类型 {@link FileType}
      */
     public static int fileType(String fileName) {
+
         //未知类型
         int fileType = FileType.UNKNOWN;
         String typeStr = getFileExtension(fileName);
@@ -623,7 +679,6 @@ public class FileUtil2 {
      *
      * @param pathname 文件路径
      * @return 文件的md5，如果中间出现异常，则返回""
-     * @throws FileNotFoundException
      */
     public static String md5(String pathname) {
         checkThread("md5");
@@ -682,7 +737,7 @@ public class FileUtil2 {
         if (!".zip".equalsIgnoreCase(getFileExtension(outputZipPathName))) {
             logW("zipCompress方法的参数outputPath最好以.zip结尾");
         }
-        ZipOutputStream zipOut = null;
+        ZipOutputStream zipOut;
         BufferedOutputStream bos = null;
         try {
             File inputFile = new File(inputPath);
@@ -690,8 +745,6 @@ public class FileUtil2 {
             bos = new BufferedOutputStream(zipOut);
             zipCompressRecursive(zipOut, inputFile, inputFile.getName(), keepEmptyDictionary);
             return true;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -773,7 +826,10 @@ public class FileUtil2 {
                 if (outPath.indexOf('/') >= 0) {
                     File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
                     if (!file.exists()) {
-                        file.mkdirs();
+                        if (!file.mkdirs()) {
+                            logW("unzip方法创建路径失败，path=" + inputZipPath);
+                            return false;
+                        }
                     }
                 }
                 if (new File(outPath).isDirectory()) {
